7f30225d7a9feedffcfffadd98caa2d98fc55acb
luetkemj <luetkemj@gmail.com>
2023-01-13 16:19:35 -0700

    deploy script for github pages

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { setupCounter } from './counter'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="/bonepick/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
