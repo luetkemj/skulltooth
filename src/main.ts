77b0f8d79d8eb1f7206d4efff70d043a4ead4a57
luetkemj <luetkemj@gmail.com>
2023-01-27 15:44:12 -0700

    format

--- src/main.ts
+++ src/main.ts
@@ -2,48 +2,48 @@ import { setupCanvas, View } from "./canvas";
 import "./style.css";
 
 const init = async () => {
-    await setupCanvas(document.querySelector<HTMLCanvasElement>("#canvas")!);
+  await setupCanvas(document.querySelector<HTMLCanvasElement>("#canvas")!);
 
-    new View({
-        width: 12,
-        height: 2,
+  new View({
+    width: 12,
+    height: 2,
+    x: 0,
+    y: 0,
+    layers: 2,
+    tileSets: ["tile", "text"],
+    tints: [0xffffff, 0xff0077],
+    alphas: [1, 1],
+    template: [
+      {},
+      {
+        string: "Skulltooth",
+        x: 0,
+        y: 1,
+      },
+    ],
+  });
+
+  new View({
+    width: 12,
+    height: 1,
+    x: 0,
+    y: 43,
+    layers: 2,
+    tileSets: ["tile", "text"],
+    tints: [0xff0077, 0xff77f],
+    alphas: [1, 1],
+    template: [
+      {
+        string: "TAG: GITHASH",
+        layer: 1,
         x: 0,
         y: 0,
-        layers: 2,
-        tileSets: ["tile", "text"],
-        tints: [0xffffff, 0xff0077],
-        alphas: [1, 1],
-        template: [
-            {},
-            {
-                string: "Skulltooth",
-                x: 0,
-                y: 1,
-            },
-        ],
-    });
-
-    new View({
-        width: 12,
-        height: 1,
-        x: 0,
-        y: 43,
-        layers: 2,
-        tileSets: ["tile", "text"],
-        tints: [0xff0077, 0xff77f],
-        alphas: [1, 1],
-        template: [
-            {
-                string: "TAG: GITHASH",
-                layer: 1,
-                x: 0,
-                y: 0,
-                tileSet: "text",
-                tint: 0xffffff,
-                alpha: 1,
-            },
-        ],
-    });
+        tileSet: "text",
+        tint: 0xffffff,
+        alpha: 1,
+      },
+    ],
+  });
 };
 
 init();
