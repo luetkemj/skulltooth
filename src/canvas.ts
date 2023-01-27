0c18ea23f13f636fdd82722170acb8f94a507b40
luetkemj <luetkemj@gmail.com>
2023-01-27 15:22:39 -0700

    rafactor

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,11 +1,11 @@
 import _ from "lodash";
 import {
-  Application,
-  Assets,
-  Container,
-  Sprite,
-  Spritesheet,
-  Texture,
+    Application,
+    Assets,
+    Container,
+    Sprite,
+    Spritesheet,
+    Texture,
 } from "pixi.js";
 import { menloBoldAlphaMap as asciiMap } from "./sprite-maps/menlo-bold.map";
 import { menloBoldHalfAlphaMap as fontMap } from "./sprite-maps/menlo-bold-half.map";
@@ -13,258 +13,286 @@ import { menloBoldHalfAlphaMap as fontMap } from "./sprite-maps/menlo-bold-half.
 let app: Application;
 
 const grid = {
-  width: 100,
-  height: 44,
+    width: 100,
+    height: 44,
 };
 
 const cellWidth = window.innerWidth / grid.width;
 // const cellHfW = cellWidth / 2;
 
 type Textures = {
-  ascii: Spritesheet;
-  text: Spritesheet;
-  tile: Texture;
+    ascii: Spritesheet;
+    text: Spritesheet;
+    tile: Texture;
 };
 const textures = {} as Textures;
 
 export async function setupCanvas(element: HTMLCanvasElement): Promise<void> {
-  app = new Application({
-    view: element,
-    width: window.innerWidth,
-    height: cellWidth * grid.height,
-    autoDensity: true,
-    resolution: window.devicePixelRatio || 1,
-    // TODO: allow user resizing
-    // resizeTo is a prop that can eventually be used for proper resizing
-  });
+    app = new Application({
+        view: element,
+        width: window.innerWidth,
+        height: cellWidth * grid.height,
+        autoDensity: true,
+        resolution: window.devicePixelRatio || 1,
+        // TODO: allow user resizing
+        // resizeTo is a prop that can eventually be used for proper resizing
+    });
 
-  await loadSprites();
+    await loadSprites();
 }
 
 export const loadSprites = async (): Promise<Textures> => {
-  textures.ascii = await Assets.load("/skulltooth/fonts/menlo-bold.json");
-  textures.text = await Assets.load("/skulltooth/fonts/menlo-bold-half.json");
-  textures.tile = await Assets.load("/skulltooth/tile.png");
+    textures.ascii = await Assets.load("/skulltooth/fonts/menlo-bold.json");
+    textures.text = await Assets.load("/skulltooth/fonts/menlo-bold-half.json");
+    textures.tile = await Assets.load("/skulltooth/tile.png");
 
-  return textures
+    return textures;
 };
 
 const getAsciiTexture = (char: string): Texture => {
-  return textures.ascii.textures[asciiMap[char as keyof typeof asciiMap]];
+    return textures.ascii.textures[asciiMap[char as keyof typeof asciiMap]];
 };
 
 const getFontTexture = (char: string): Texture => {
-  return textures.text.textures[fontMap[char as keyof typeof fontMap]];
+    return textures.text.textures[fontMap[char as keyof typeof fontMap]];
 };
 
 const getTileTexture = (): Texture => {
-  return textures.tile;
+    return textures.tile;
 };
 
 interface ViewOptions {
-  width: number;
-  height: number;
-  halfWidth: boolean;
-  x: number;
-  y: number;
-  char: string;
-  layers: number;
-  tileSet: string;
-  tint: number;
-  alpha: number;
+    width: number;
+    height: number;
+    x: number;
+    y: number;
+    layers: number;
+    tileSets: Array<string>;
+    tints: Array<number>;
+    alphas: Array<number>;
+    template?: Array<TemplateRow>;
 }
 
 interface GetTextureOptions {
-  tileSet: string;
-  char: string;
+    tileSet: string;
+    char: string;
 }
 
 interface CreateSpriteOptions {
-  char: string;
-  width: number;
-  height: number;
-  halfWidth: Boolean;
-  layer: number;
-  x: number;
-  y: number;
-  tileSet: string;
-  tint: number;
-  alpha: number;
+    char: string;
+    width: number;
+    height: number;
+    layer: number;
+    x: number;
+    y: number;
+    tileSet: string;
+    tint: number;
+    alpha: number;
 }
 
 interface UpdateSprite {
-  char?: string;
-  layer: number;
-  x: number;
-  y: number;
-  tileSet: string;
-  tint: number;
-  alpha: number;
+    char?: string;
+    layer: number;
+    x: number;
+    y: number;
+    tileSet: string;
+    tint: number;
+    alpha: number;
+}
+
+interface TemplateRow {
+    string?: string;
+    layer?: number;
+    x?: number;
+    y?: number;
+    tileSet?: string;
+    tint?: number;
+    alpha?: number;
 }
 
 interface UpdateRow {
-  string: string;
-  layer: number;
-  x: number;
-  y: number;
-  tileSet: string;
-  tint: number;
-  alpha: number;
+    string?: string;
+    layer: number;
+    x?: number;
+    y?: number;
+    tileSet?: string;
+    tint?: number;
+    alpha?: number;
 }
 
 type Layer = {
-  char: string;
-  tint: number;
-  alpha: number;
-  tileSet: string;
-  x: number;
-  y: number;
+    char: string;
+    tint: number;
+    alpha: number;
+    tileSet: string;
+    x: number;
+    y: number;
 };
 
 type LayerMap = { [key: string]: Layer };
 
 export class View {
-  width;
-  height;
-  layers: Container[] = [];
-  sprites: Sprite[][][] = [];
+    halfWidth: Boolean = false;
+    width;
+    height;
+    layers: Container[] = [];
+    sprites: Sprite[][][] = [];
+    tileSets: Array<string> = [];
+    tints: Array<number> = [];
+    alphas: Array<number> = [];
 
-  constructor(options: ViewOptions) {
-    this.width = options.width;
-    this.height = options.height;
+    constructor(options: ViewOptions) {
+        this.width = options.width;
+        this.height = options.height;
+        this.tileSets = options.tileSets;
+        this.tints = options.tints;
+        this.alphas = options.alphas;
+        this.halfWidth = this.tileSets.includes("text");
 
-    // create n layers of containers
-    _.times(options.layers, () => this.layers.push(new Container()));
+        // create n layers of containers
+        _.times(options.layers, () => this.layers.push(new Container()));
 
-    this.layers.forEach((layer) => {
-      const posX = options.halfWidth
-        ? options.x * (cellWidth / 2)
-        : options.x * cellWidth;
-      const posY = options.y * cellWidth;
+        this.layers.forEach((layer) => {
+            const posX = options.halfWidth
+                ? options.x * (cellWidth / 2)
+                : options.x * cellWidth;
+            const posY = options.y * cellWidth;
 
-      layer.x = posX;
-      layer.y = posY;
-      layer.interactiveChildren = false;
+            layer.x = posX;
+            layer.y = posY;
+            layer.interactiveChildren = false;
 
-      app.stage.addChild(layer);
-    });
+            app.stage.addChild(layer);
+        });
 
-    // create three layers of arrays of arrays to store sprites
-    _.times(options.layers, () =>
-      this.sprites.push(
-        Array.from(Array(this.height), () => Array.from(Array(this.width)))
-      )
-    );
+        // create three layers of arrays of arrays to store sprites
+        _.times(options.layers, () =>
+            this.sprites.push(
+                Array.from(Array(this.height), () => Array.from(Array(this.width)))
+            )
+        );
 
-    // create sprites and store them
-    _.times(options.layers, (layer) => {
-      _.times(this.height, (y) => {
-        _.times(this.width, async (x) => {
-          this._createSprite({
-            char: options.char,
-            width: cellWidth,
-            height: cellWidth,
-            halfWidth: options.halfWidth,
+        // create sprites and store them
+        _.times(options.layers, (layer) => {
+            _.times(this.height, (y) => {
+                _.times(this.width, async (x) => {
+                    this._createSprite({
+                        char: "",
+                        width: cellWidth,
+                        height: cellWidth,
+                        layer,
+                        x,
+                        y,
+                        tileSet: options.tileSets[layer],
+                        tint: options.tints[layer],
+                        alpha: options.alphas[layer],
+                    });
+                });
+            });
+        });
+
+        if (options.template) {
+            options.template.forEach((row, index) => {
+                this.updateRow({
+                    string: row.string || "",
+                    layer: index,
+                    x: row.x || 0,
+                    y: row.y || 0,
+                    tileSet: row.tileSet || this.tileSets[index],
+                    tint: row.tint || this.tints[index],
+                    alpha: row.alpha || this.alphas[index],
+                });
+            });
+        }
+
+        return this;
+    }
+
+    _getTexture = (opts: GetTextureOptions): Texture => {
+        const { tileSet, char } = opts;
+        if (tileSet === "ascii") return getAsciiTexture(char);
+        if (tileSet === "text") return getFontTexture(char);
+        return getTileTexture();
+    };
+
+    _createSprite = (opts: CreateSpriteOptions) => {
+        const {
+            char,
+            width,
+            height,
             layer,
             x,
             y,
-            tileSet: options.tileSet,
-            tint: options.tint,
-            alpha: options.alpha,
-          });
+            tileSet,
+            tint = 0xffffff,
+            alpha = 1,
+        } = opts;
+
+        const texture = this._getTexture({ tileSet, char });
+        let sprite = new Sprite(texture);
+        sprite.width = this.halfWidth ? width / 2 : width;
+        sprite.height = height;
+        sprite.x = this.halfWidth ? x * (width / 2) : x * width;
+        sprite.y = y * height;
+        sprite.tint = tint;
+        sprite.alpha = alpha;
+
+        this.sprites[layer][y][x] = sprite;
+        this.layers[layer].addChild(sprite);
+    };
+
+    updateCell = (layerMap: LayerMap) => {
+        Object.keys(layerMap).forEach((layer) => {
+            const { char, tint, alpha, tileSet, x, y } = layerMap[layer];
+
+            this.updateSprite({
+                char,
+                tint,
+                alpha,
+                tileSet,
+                x,
+                y,
+                layer: parseInt(layer),
+            });
         });
-      });
-    });
+    };
 
-    return this;
-  }
+    updateSprite = async (opts: UpdateSprite) => {
+        const { char = "", layer, x, y, tileSet = "text", tint, alpha } = opts;
+        const sprite = this.sprites[layer][y][x];
+        sprite.texture = this._getTexture({ tileSet, char });
+        if (tint) sprite.tint = tint;
+        if (alpha) sprite.alpha = alpha;
+    };
 
-  _getTexture = (opts: GetTextureOptions): Texture => {
-    const { tileSet, char } = opts;
-    if (tileSet === "ascii") return getAsciiTexture(char);
-    if (tileSet === "text") return getFontTexture(char);
-    return getTileTexture();
-  };
+    updateRow = (opts: UpdateRow) => {
+        const { string, layer, x, y, tileSet, tint, alpha } = opts;
+        if (string) {
+            [...string].forEach((char, index) =>
+                this.updateSprite({
+                    char,
+                    layer,
+                    x: x || 0 + index,
+                    y: y || 0,
+                    tileSet: tileSet || this.tileSets[layer],
+                    tint: tint || this.tints[layer],
+                    alpha: alpha || this.alphas[layer],
+                })
+            );
+        }
 
-  _createSprite = (opts: CreateSpriteOptions) => {
-    const {
-      char,
-      width,
-      height,
-      halfWidth,
-      layer,
-      x,
-      y,
-      tileSet,
-      tint = 0xffffff,
-      alpha = 1,
-    } = opts;
+        if (tileSet === "tile")
+            _.times(this.width, (index) => {
+                this.updateSprite({
+                    layer,
+                    x: x || 0 + index,
+                    y: y || 0,
+                    tileSet: tileSet || this.tileSets[layer],
+                    tint: tint || this.tints[layer],
+                    alpha: alpha || this.alphas[layer],
+                });
+            });
 
-    const texture = this._getTexture({ tileSet, char });
-    let sprite = new Sprite(texture);
-    sprite.width = halfWidth ? width / 2 : width;
-    sprite.height = height;
-    sprite.x = halfWidth ? x * (width / 2) : x * width;
-    sprite.y = y * height;
-    sprite.tint = tint;
-    sprite.alpha = alpha;
-
-    this.sprites[layer][y][x] = sprite;
-    this.layers[layer].addChild(sprite);
-  };
-
-  updateCell = (layerMap: LayerMap) => {
-    Object.keys(layerMap).forEach((layer) => {
-      const { char, tint, alpha, tileSet, x, y } = layerMap[layer];
-
-      this.updateSprite({
-        char,
-        tint,
-        alpha,
-        tileSet,
-        x,
-        y,
-        layer: parseInt(layer),
-      });
-    });
-  };
-
-  updateSprite = async (opts: UpdateSprite) => {
-    const { char = "", layer, x, y, tileSet = "text", tint, alpha } = opts;
-    const sprite = this.sprites[layer][y][x];
-    sprite.texture = this._getTexture({ tileSet, char });
-    if (tint) sprite.tint = tint;
-    if (alpha) sprite.alpha = alpha;
-  };
-
-  updateRow = (opts: UpdateRow) => {
-    const { string, layer, x, y, tileSet, tint, alpha } = opts;
-    if (string) {
-      [...string].forEach((char, index) =>
-        this.updateSprite({
-          char,
-          layer,
-          x: x + index,
-          y,
-          tileSet,
-          tint,
-          alpha,
-        })
-      );
-    }
-
-    if (tileSet === "tile")
-      _.times(this.width, (index) => {
-        this.updateSprite({
-          layer,
-          x: x + index,
-          y,
-          tileSet,
-          tint,
-          alpha,
-        });
-      });
-
-    return this;
-  };
+        return this;
+    };
 }
--- src/main.ts
+++ src/main.ts
@@ -4,88 +4,45 @@ import "./style.css";
 const init = async () => {
     await setupCanvas(document.querySelector<HTMLCanvasElement>("#canvas")!);
 
-    const view = new View({
-        width: 13,
-        height: 3,
+    new View({
+        width: 12,
+        height: 2,
         x: 0,
         y: 0,
-        halfWidth: false,
-        char: " ",
-        layers: 4,
-        tileSet: "ascii",
-        tint: 0xfff77f,
-        alpha: 1,
+        layers: 2,
+        tileSets: ["tile", "text"],
+        tints: [0xffffff, 0xff0077],
+        alphas: [1, 1],
+        template: [
+            {},
+            {
+                string: "Skulltooth",
+                x: 0,
+                y: 1,
+            },
+        ],
     });
 
-    view.updateRow({
-        string: "SkulltoothFC",
-        layer: 0,
-        x: 0,
-        y: 0,
-        tileSet: "tile",
-        tint: 0xffffff,
-        alpha: 1,
-    });
-
-    view.updateRow({
-        string: " Skulltooth",
-        layer: 1,
-        x: 0,
-        y: 0,
-        tileSet: "ascii",
-        tint: 0xff0077,
-        alpha: 1,
-    });
-
-    view.updateRow({
-        string: "Force Crusher",
-        layer: 0,
-        x: 0,
-        y: 1,
-        tileSet: "tile",
-        tint: 0xff0077,
-        alpha: 1,
-    });
-
-    view.updateRow({
-        string: "Force Crusher",
-        layer: 1,
-        x: 0,
-        y: 1,
-        tileSet: "ascii",
-        tint: 0xffffff,
-        alpha: 1,
-    });
-
-    view.updateCell({
-        0: { char: "?", tint: 0x0e0e0e, alpha: 1, tileSet: "tile", x: 0, y: 0 },
-        1: { char: "?", tint: 0xc0ffee, alpha: 1, tileSet: "ascii", x: 0, y: 0 },
-        2: { char: "!", tint: 0xc0ffee, alpha: 1, tileSet: "ascii", x: 0, y: 0 },
-        3: { char: "!", tint: 0xc0ffee, alpha: 0.5, tileSet: "tile", x: 0, y: 0 },
-    });
-
-    // add an initial string or template so we don't have to call updateRow right away.
-    const viewGitHash = new View({
-        width: 13,
+    new View({
+        width: 12,
         height: 1,
         x: 0,
         y: 43,
-        halfWidth: true,
-        char: " ",
-        layers: 1,
-        tileSet: "text",
-        tint: 0xfff77f,
-        alpha: 1,
-    });
-
-    viewGitHash.updateRow({
-        string: "TAG: GITHASH",
-        layer: 0,
-        x: 0,
-        y: 0,
-        tileSet: "text",
-        tint: 0xffffff,
-        alpha: 1,
+        layers: 2,
+        tileSets: ["tile", "text"],
+        tints: [0xff0077, 0xff77f],
+        alphas: [1, 1],
+        template: [
+            {
+                string: "TAG: GITHASH",
+                layer: 1,
+                x: 0,
+                y: 0,
+                tileSet: "text",
+                tint: 0xffffff,
+                alpha: 1,
+            },
+        ],
     });
 };
 
