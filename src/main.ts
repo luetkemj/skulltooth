12fd2298f506037c212d0f9c859a39472d96f87b
luetkemj <luetkemj@gmail.com>
2023-01-29 09:41:32 -0700

    updateRows

--- src/main.ts
+++ src/main.ts
@@ -2,46 +2,32 @@ import { setupCanvas, View } from "./canvas";
 import "./style.css";
 
 const init = async () => {
-  await setupCanvas(document.querySelector<HTMLCanvasElement>("#canvas")!);
+    await setupCanvas(document.querySelector<HTMLCanvasElement>("#canvas")!);
 
-  new View({
-    width: 12,
-    height: 2,
-    x: 0,
-    y: 0,
-    layers: 2,
-    tileSets: ["tile", "text"],
-    tints: [0xffffff, 0xff0077],
-    alphas: [1, 1],
-    template: [
-      {},
-      {
-        string: "Skulltooth",
-        x: 0,
-        y: 1,
-      },
-    ],
-  });
-
-  new View({
-    width: 12,
-    height: 1,
-    x: 0,
-    y: 43,
-    layers: 2,
-    tileSets: ["tile", "ascii"],
-    tints: [0xff0077, 0xffffff],
-    alphas: [1, 1],
-    template: [
-      {},
-      {
-        string: "TAG: GITHASH",
-        layer: 1,
+    new View({
+        width: 12,
+        height: 2,
         x: 0,
         y: 0,
-      },
-    ],
-  });
+        layers: 2,
+        tileSets: ["tile", "text"],
+        tints: [0xffffff, 0xff0077],
+        alphas: [1, 1],
+    }).updateRows([
+        [{}, { string: " skulltooth" }],
+        [{ tint: 0xff0077 }, { string: "forcecrusher", tint: 0xffffff }],
+    ]);
+
+    new View({
+        width: 12,
+        height: 1,
+        x: 0,
+        y: 43,
+        layers: 1,
+        tileSets: ["text"],
+        tints: [0xffffff],
+        alphas: [1],
+    }).updateRows([[{ string: "TAG: GITHASH" }]]);
 };
 
 init();
