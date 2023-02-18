7bb50b3a4effee203975f3de2e2f10c56ecb86cd
luetkemj <luetkemj@gmail.com>
2023-02-18 07:53:37 -0700

    remove engine and tests for rewrite

--- src/engine/index.js
+++
@@ -1,7 +0,0 @@
-export { Component } from "./Component";
-export { ComponentRegistry } from "./ComponentRegistry";
-export { Engine } from "./Engine";
-export { Entity } from "./Entity";
-export { EntityEvent } from "./EntityEvent";
-export { Query } from "./Query";
-export { World } from "./World";
