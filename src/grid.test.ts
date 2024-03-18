3114062d27fb836f78d3e357f29ccf44dad8b993
luetkemj <luetkemj@gmail.com>
2024-03-18 17:09:22 -0600

    circle and inside circle functions with tests

--- src/grid.test.ts
+++ src/grid.test.ts
@@ -1,5 +1,5 @@
-import { describe, test, expect } from "vitest";
-import { toPosId, toPos } from "./grid";
+import { describe, test, expect, beforeEach } from "vitest";
+import { circle, insideCircle, toPosId, toPos } from "./grid";
 
 describe("grid", () => {
   describe("toPosId", () => {
@@ -11,6 +11,81 @@ describe("grid", () => {
   describe("toPos", () => {
     test("should work", () => {
       expect(toPos("1,2,3")).toEqual({ x: 1, y: 2, z: 3 });
+    });
+  });
+
+  describe("circle", () => {
+    test("should work", () => {
+      const c = circle({ x: 5, y: 5, z: 1 }, 3.5);
+      expect(c).toEqual({
+        posIds: [
+          "4,2,1",
+          "5,2,1",
+          "6,2,1",
+          "3,3,1",
+          "4,3,1",
+          "5,3,1",
+          "6,3,1",
+          "7,3,1",
+          "2,4,1",
+          "3,4,1",
+          "4,4,1",
+          "5,4,1",
+          "6,4,1",
+          "7,4,1",
+          "8,4,1",
+          "2,5,1",
+          "3,5,1",
+          "4,5,1",
+          "5,5,1",
+          "6,5,1",
+          "7,5,1",
+          "8,5,1",
+          "2,6,1",
+          "3,6,1",
+          "4,6,1",
+          "5,6,1",
+          "6,6,1",
+          "7,6,1",
+          "8,6,1",
+          "3,7,1",
+          "4,7,1",
+          "5,7,1",
+          "6,7,1",
+          "7,7,1",
+          "4,8,1",
+          "5,8,1",
+          "6,8,1",
+        ],
+        center: "5,5,1",
+        diameter: 7,
+        top: 1.5,
+        bottom: 8.5,
+        left: 1.5,
+        right: 8.5,
+      });
+    });
+  });
+
+  describe("insideCircle", () => {
+    let c;
+    const center = { x: 5, y: 5, z: 1 };
+    const radius = 3.5
+
+    beforeEach(() => {
+      c = circle(center, radius);
+    });
+
+    test("should return true if candidate is inside circle", () => {
+      expect(insideCircle(center, radius, {x: 4, y: 2, z: 1})).toBeTruthy();
+    });
+
+    test("should return false if candidate is outside circle", () => {
+      expect(insideCircle(center, radius, {x: 0, y: 0, z: 1})).toBeFalsy();
+    });
+
+    test("should ignore z", () => {
+      expect(insideCircle(center, radius, {x: 4, y: 2, z: 3})).toBeTruthy();
     });
   });
 });
--- src/grid.ts
+++ src/grid.ts
@@ -1,11 +1,51 @@
 type PosId = string;
-type Pos = { x: number, y: number, z: number}
+type Pos = { x: number; y: number; z: number };
 
 export const toPosId = (pos: Pos): PosId => {
-    return `${pos.x},${pos.y},${pos.z}`
-}
+  return `${pos.x},${pos.y},${pos.z}`;
+};
 
 export const toPos = (posId: PosId): Pos => {
-    const coords = posId.split(',').map(coord => parseInt(coord, 10))
-    return {  x: coords[0],  y: coords[1], z: coords[2] }
-}
+  const coords = posId.split(",").map((coord) => parseInt(coord, 10));
+  return { x: coords[0], y: coords[1], z: coords[2] };
+};
+
+export const insideCircle = (center: Pos, radius: number, candidate: Pos): Boolean => {
+  const dx = center.x - candidate.x;
+  const dy = center.y - candidate.y;
+  const distance_squared = dx * dx + dy * dy;
+  return distance_squared <= radius * radius;
+};
+
+export const circle = (center: Pos, radius: number) => {
+  const diameter = radius % 1 ? radius * 2 : radius * 2 + 1;
+  const top = center.y - radius;
+  const bottom = center.y + radius;
+  const left = center.x - radius;
+  const right = center.x + radius;
+
+  const posIds: Array<PosId> = [];
+
+  for (let y = top; y <= bottom; y++) {
+    for (let x = left; x <= right; x++) {
+      const cx = Math.ceil(x);
+      const cy = Math.ceil(y);
+      const cz = center.z;
+
+      const candidate = { x: cx, y: cy, z: cz };
+      if (insideCircle(center, radius, candidate)) {
+        posIds.push(toPosId(candidate));
+      }
+    }
+  }
+
+  return {
+    posIds,
+    center: toPosId(center),
+    diameter,
+    top,
+    bottom,
+    left,
+    right,
+  };
+};
